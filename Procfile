779f6db94b93d62ef1564b75d3ccdacd234f89e9
Thomasrens <thomas.rens04@gmail.com>
2026-05-30 19:08:41 +0530

    Add STATIC_ROOT

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rentiz.wsgi:application
--- rentiz/settings.py
+++ rentiz/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/' 
+import os
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=(str(BASE_DIR.joinpath('static')),) 
 MEDIA_URL='/media/' 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
